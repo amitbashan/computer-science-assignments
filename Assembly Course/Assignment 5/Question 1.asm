e396e4123f4ec7cbf125e6748fd14704ae765ce1
Amit Bashan <amitxbashan@gmail.com>
2020-10-11 16:31:28 +0300

    Added Assignment 5

---
+++ Assembly Course/Assignment 5/Question 1.asm
@@ -0,0 +1,56 @@
+.model small
+.stack 100h
+
+.data
+	number dw -2584
+
+.code
+	mov ax, @data
+	mov ds, ax
+	
+	push number
+	call print_signed_number
+	jmp _exit
+
+	print_signed_number PROC NEAR
+		push bp
+		mov bp, sp
+		mov ax, [bp + 4]
+		mov si, ax
+		mov bx, 10
+		xor cx, cx
+		cmp ax, 0
+		jge next
+		neg ax
+
+		next:
+			xor dx, dx
+			div bx
+			add dx, '0'
+			push dx
+			inc cx
+			cmp ax, 0
+			jne next
+			cmp si, 0
+			jge _end
+			push '-'
+			inc cx 
+
+		_end:
+			cmp cx, 0
+			je return
+			pop dx
+			mov ah, 2
+			int 21h
+			dec cx
+			jmp _end
+
+			return:
+				pop bp
+				ret 2
+		
+		print_signed_number ENDP
+
+	_exit:
+		.exit
+end
---
+++ Assembly Course/Assignment 5/Question 2.1.asm
@@ -0,0 +1,30 @@
+.model small
+.stack 100h
+
+.data
+	A dw 12, 5, 8, -1, 4
+	B dw -2, 9, 0, 18, 3
+	C dw 5 dup (?)
+
+.code
+	mov ax, @data
+	mov ds, ax
+
+	push A ; 12
+	push B ; -2
+	call signed_sum ; AX = 0xA
+	jmp _exit
+
+	; Result will stored in AX.
+	signed_sum PROC NEAR
+		push bp
+		mov bp, sp
+		mov ax, [bp + 4]
+		add ax, [bp + 6]
+		pop bp
+		ret 4
+	signed_sum ENDP
+
+	_exit:
+		.exit
+end
