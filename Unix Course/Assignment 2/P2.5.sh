d1b3146fd8a0c3e63330727345164695ea8094ef
Amit Bashan <amitxbashan@gmail.com>
2020-11-24 14:04:09 +0200

    Indentation fix.

--- Unix Course/Assignment 2/P2.5.sh
+++ Unix Course/Assignment 2/P2.5.sh
@@ -5,11 +5,11 @@ read names
 
 for name in $names; do
 	if [[ -d $name ]]; then
-    	echo "$name is a directory"
+		echo "$name is a directory"
 	elif [[ -f $name ]]; then
-    	echo -n "$name is a regular file of size "
+		echo -n "$name is a regular file of size "
 		echo $(wc -c < $name)
 	else
-    	echo "$name is not a file or directory"
+		echo "$name is not a file or directory"
 	fi
 done
